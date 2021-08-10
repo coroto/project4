ef0134e704483fbbd97b47d3c5a75f9a3788ae8f
Juancho <juancho.coroto@gmail.com>
2021-08-11 02:16:07 +1000

    database creation and project structure

---
+++ index.js
@@ -0,0 +1,34 @@
+const express=require("express")
+const app=express()
+
+const PORT = process.env.PORT  || 3000
+
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//View engine
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+// set 'public' folder as static folder
+app.use(express.static(__dirname + '/public'))
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.render('index', {title: 'Mr Coffee'})
+});
+
+
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {
+    title: "404",
+    // description: "req.url"
+    description: "Page not found"
+  })
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listener at http://localhost:${PORT}`);
+  });
---
+++ sql/create-tables.sql
@@ -0,0 +1,25 @@
+\c mrcoffee;
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(60) NOT NULL
+);
+
+DROP TABLE IF EXISTS schedules;
+
+CREATE TABLE IF NOT EXISTS schedules (
+  id SERIAL PRIMARY KEY,
+  id_user INTEGER NOT NULL,
+  day INTEGER CHECK (day >= 1 AND day <= 7),
+  start_time TIMESTAMPTZ NOT NULL,
+  end_time TIMESTAMPTZ NOT NULL,
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(id_user)
+    REFERENCES users(id)
+    ON DELETE CASCADE
+);
